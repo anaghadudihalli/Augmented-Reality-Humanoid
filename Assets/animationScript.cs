6f1580efb6a79235ca766a72c849d19e900a507a
Anagha Dudihalli <adudih2@uic.edu>
2020-10-10 18:22:24 -0500

    Add button

---
+++ Assets/animationScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using Vuforia;
+using TMPro;
+
+public class animationScript : MonoBehaviour
+{
+    public GameObject buttonObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonObject = GameObject.Find("buttonToSpeak");
+        buttonObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);   
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Button Pressed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
