c113e5b9d6615e1324d04cfcfae72f09dbd7c5bf
Anagha Dudihalli <adudih2@uic.edu>
2020-10-10 20:39:03 -0500

    Change animations and speak

--- Assets/animationScript.cs
+++ Assets/animationScript.cs
@@ -8,16 +8,24 @@ using TMPro;
 public class animationScript : MonoBehaviour
 {
     public GameObject buttonObject;
+    public Animator anim;
+    public AudioSource source;
+    public AudioClip clip;
     // Start is called before the first frame update
     void Start()
     {
         buttonObject = GameObject.Find("buttonToSpeak");
         buttonObject.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);   
+        anim.GetComponent<Animator>();
     }
 
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
         Debug.Log("Button Pressed");
+        Debug.Log("*********");
+        Debug.Log("*********");
+        anim.SetBool("isTalking", true);
+        source.PlayOneShot(clip);
     }
 
     // Update is called once per frame
