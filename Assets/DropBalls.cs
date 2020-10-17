97c85474855e50061aac05041b1877e40817feeb
Anagha Dudihalli <adudih2@uic.edu>
2020-10-17 14:41:29 -0500

    Falling balls - goes through the floor for now

---
+++ Assets/DropBalls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropBalls : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform startPosRot;
+	public GameObject ball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	Vector3 posAdd;
+
+     if (Input.GetMouseButton(0)) {
+     	
+     	posAdd = new Vector3(Random.Range(-0.09f, 0.09f), Random.Range(-0.06f, 0.06f), Random.Range(-0.06f, 0.06f));
+
+     	Instantiate(ball, startPosRot.position+posAdd, startPosRot.rotation);
+     }   
+    }
+}
